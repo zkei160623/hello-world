06cc30ecdc9719fb180ddde6f1ddd64e80530c2b
zkei <zkei01@gmail.com>
2024-01-21 11:42:26 +0900

    fix develop program

--- abc.tsx
+++ abc.tsx
@@ -2,3 +2,4 @@ console.log("hello,001.test changed.");
 console.log("hello,002.fixed on local pc.");
 console.log("add one line.");
 console.log("add second line.");
+console.log("add third line.");
